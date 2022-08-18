21fb3dc5d57f796a7ba6b2fe918fb39187273f29
Jack Guthrie <jackg3323@gmail.com>
2022-08-18 19:35:21 +0100

    Create snapshot test and change handler test

---
+++ src/components/Input/Input.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import renderer from "react-test-renderer";
+import Input from "./Input";
+
+it("renders correctly", () => {
+  const tree = renderer.create(
+    <Input label="" value={0} changeHandler={() => {}} />
+  );
+  expect(tree).toMatchSnapshot();
+});
+
+it("call changeHandler whenever input is changed", () => {
+  const changeHandler = jest.fn();
+
+  render(<Input label="celsius" value={0} changeHandler={changeHandler} />);
+  const input = screen.getByLabelText("celsius");
+  userEvent.type(input, "1");
+  expect(changeHandler).toHaveBeenCalledTimes(1);
+});
